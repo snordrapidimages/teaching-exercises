e4d4694e7806a486df52040e6045ffc08094c77f
Simon Nord <snord@rapidimages.se>
2015-08-27 12:04:15 +0200

    solution

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
 // Require your modules here
+var http = require('http');
+var fs = require('fs');
 
 // Write your code here
+
+require('./lib/buildClientAssets');
+
+var server = http.createServer(function(request, response) {
+
+  switch(request.url) {
+    case "/":
+    case "/index.html":
+      response.writeHead(200, {
+        contentType: "text/html"
+      });
+
+      fs.readFile('./index.html', function(err, data){
+        if(err) throw err;
+
+        response.end(data);
+      });
+      break;
+    case "/app.js":
+      response.writeHead(200, {
+        contentType: "application/x-javascript"
+      });
+
+      fs.createReadStream('./clientside/dist/app.js').pipe(response);
+      break;
+
+  }
+
+});
+
+server.listen(8000)
